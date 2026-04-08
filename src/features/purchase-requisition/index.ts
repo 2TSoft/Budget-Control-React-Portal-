c1518cd450c767e3908452f53392aa53241d57e0
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2026-04-08 09:51:53 +0700

    - Clear Code

--- src/features/purchase-requisition/index.ts
+++
@@ -1,2 +0,0 @@
-export type { PRHeader, PRLine, PRHeaderFormData, PRLineFormData } from './types';
-export { PRStatus, PRLineStatus } from './types';
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'antd/dist/reset.css'
 import './index.css'
-import './styles/inclusive-design.css'
-import { AppProviders } from './app/providers.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppProviders />
+    <App />
   </StrictMode>,
 )
